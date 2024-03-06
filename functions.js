6426b0a3e65bc67e4602a72ba4b66d1efac8610f
Csaba Czuth <oneandonlyheliboy@gmail.com>
2024-03-06 23:20:47 +0000

    Remove unnecessary html and css

--- functions.js
+++ functions.js
@@ -1,16 +1,17 @@
 //define variables
 const gridNumber = document.querySelector("#gridNumber");
-//set default grid size
-gridNumber.value = 6;
-let value = parseInt(gridNumber.value);
-//noting change in color value
 let currentColor = document.querySelector("#colorPicker").value;
-
-
 const screenSize = document.querySelector("#sketchSize");
 const container = document.querySelector("#container");
 let colorPicker = document.querySelector("#colorPicker");
 
+//set default grid size
+gridNumber.value = 16;
+
+let value = parseInt(gridNumber.value);
+
+
+//noting change in color value
 colorPicker.addEventListener("change", function(e){
          let colorValue = colorPicker.value;
          currentColor =colorValue;
@@ -33,16 +34,33 @@ for (i = 0; i < rows.length; i++) {
     for(j= 0; j < value; j++) {
         const div = document.createElement("div");
         div.classList.add("square");
+        div.id = `${i}${j}`;
         rows[i].appendChild(div);
     }
 
 }
 
 
+const grids = document.querySelectorAll(".square");
+
+grids.forEach(square => square.addEventListener("mouseenter", function(e){
+square.style.backgroundColor = currentColor;
+square.classList.add("opacity");
+}));
+
+
+
+
 //changing the grid number with eventlistener;
 gridNumber.addEventListener("change", function(e){
     container.innerHTML="";
     let value = gridNumber.value;
+    if(value > 100) {
+        alert ("100 is the limit! Such as life...");
+        value = 100;
+    }
+   
+    
     for(i = 0; i < value ; i++) {
         const div = document.createElement("div");
         div.classList.add("row");
@@ -54,6 +72,7 @@ gridNumber.addEventListener("change", function(e){
     for(j= 0; j < value; j++) {
         const div = document.createElement("div");
         div.classList.add("square");
+        div.id = `${i}${j}`;
         rows[i].appendChild(div);
     }
 
@@ -62,10 +81,32 @@ gridNumber.addEventListener("change", function(e){
     const squares = document.querySelectorAll(".square");
 
     squares.forEach(square => square.addEventListener("mouseenter", function(e){
+    
     square.style.backgroundColor = currentColor;
+    square.classList.add("opacity");
     }))
 
 
+})
+
+
+//changing size of the the container
+screenSize.addEventListener("change", function(){
+    const x = screenSize.value;
+    switch (x) {
+        case "100%":
+            container.style.width = "100%";
+            container.style.height = "100%";
+            break;
+        case "50%":
+            container.style.width = "50%";
+            container.style.height = "50%"; 
+            break;  
+        case "75%":
+            container.style.width = "75%";
+            container.style.height = "75%"; 
+            break;
+    }
 })
 
 
